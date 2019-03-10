763235c2074dbccc6741bc9fbce98eb234c7583d
Florian GARDY <florian.gardy@gmail.com>
2019-03-10 15:16:25 +0100

    Init JS module + Add JS quest 1

---
+++ JavaScript/BaseJS1/best-movie.js
@@ -0,0 +1,10 @@
+// best-MSPointerEvent.js
+
+// Variables definition
+const movieName = "La cité de la peur";
+const releaseYear = "1994";
+const director = "Alain Berberian";
+
+// Display a popup
+message = movieName + " a été réalisé par " + director + " en " + releaseYear;
+alert(message);
